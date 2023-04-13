ce241ce99e344f4cfa9f5e3038c624634dc8567e
Matheus Augusto <matheusalgusto_@hotmail.com>
2023-04-12 21:43:39 -0300

    git add .

--- build_container.sh
+++ build_container.sh
@@ -1,1 +1,4 @@
 docker build -t terumo-web-ui-quey-test .
+
+docker tag terumo-web-ui-quey-test terumoapp/terumo-web-ui-quey-test:latest
+docker push terumoapp/terumo-web-ui-quey-test
--- script.js
+++ script.js
@@ -16,7 +16,7 @@ imageInput.addEventListener('change', () => {
 uploadButton.addEventListener('click', async () => {
   const imageBase64 = preview.src.split(',')[1];
 
- 
+
   const response = await fetch('http://127.0.0.1:5001/image-query', {
     method: 'POST',
     headers: {
@@ -25,5 +25,52 @@ uploadButton.addEventListener('click', async () => {
     body: JSON.stringify({ image: { data: imageBase64, format: fileType } })
   });
   const result = await response.json();
-  console.log(result);
+  console.log(result.result);
+
+  
+  let resultsContainer = document.querySelector('.results-container');
+
+  // If the element doesn't exist, create it and append it to the body
+  if (resultsContainer) {
+    resultsContainer.remove();
+  } 
+  resultsContainer = document.createElement('div');
+ resultsContainer.classList.add('results-container');
+
+
+  // const resultsContainer = document.createElement('div');
+  // resultsContainer.classList.add('results-container');
+
+  result.result.results.forEach((res) => {
+    const resultItem = document.createElement('div');
+    resultItem.classList.add('result-item');
+
+    const image = document.createElement('img');
+    image.src = res.thumbnail_url;
+    image.alt = res.description;
+    image.classList.add('result-image');
+    resultItem.appendChild(image);
+
+    const title = document.createElement('h2');
+    title.innerText = res.description;
+    resultItem.appendChild(title);
+
+    const tags = document.createElement('ul');
+    tags.classList.add('result-tags');
+    res.tags.forEach((tag) => {
+      const li = document.createElement('li');
+      li.innerText = tag;
+      tags.appendChild(li);
+    });
+    resultItem.appendChild(tags);
+
+    const score = document.createElement('p');
+    score.innerText = `Score: ${res.score}`;
+    resultItem.appendChild(score);
+
+    resultsContainer.appendChild(resultItem);
+  });
+  const selectedDiv = document.querySelector('.global-caontainer');
+
+  selectedDiv.appendChild(resultsContainer);
 });
