91e1b9285a12e7bdd1a647049d66c1e0163ee809
Matheus Augusto <matheusalgusto_@hotmail.com>
2023-04-12 22:02:39 -0300

    change

--- build_container.sh
+++ build_container.sh
@@ -1,4 +1,3 @@
 docker build -t terumo-web-ui-quey-test .
 
 docker tag terumo-web-ui-quey-test terumoapp/terumo-web-ui-quey-test:latest
-docker push terumoapp/terumo-web-ui-quey-test
--- push_image.sh
+++ push_image.sh
@@ -1,2 +1,1 @@
-docker tag terumo-web-ui-quey-test terumoapp/terumo-web-ui-quey-test:latest
 docker push terumoapp/terumo-web-ui-quey-test
